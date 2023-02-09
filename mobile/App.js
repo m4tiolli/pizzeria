615ccd9a48c42e6dd1b9f5edce60fd8154d98d44
m4tiolli <gabrielmatiollif@gmail.com>
2023-02-09 13:16:09 -0300

    test

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity>
+        <Text>Olá</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
