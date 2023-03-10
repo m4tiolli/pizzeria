9ad4c9749cb8be485b7e1a655a9b9f0bc611e96c
Gabriel Matiolli <97938132+m4tiolli@users.noreply.github.com>
2023-03-10 17:20:40 +0000

    desenvolvendo tela inicial

--- mobile/App.js
+++ mobile/App.js
@@ -12,27 +12,27 @@ import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/components/Routes/Routes";
 
 function App() {
-  // const [fontsLoaded] = useFonts({
-  //   Poppins_400Regular,
-  //   Poppins_600SemiBold,
-  //   Poppins_700Bold,
-  //   Poppins_800ExtraBold,
-  // });
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+  });
 
-  // if (!fontsLoaded) {
-  //   return <Loading />;
-  // }
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
 
   return (
     <>
       <NavigationContainer>
-        <Routes></Routes>
+        <Routes />
       </NavigationContainer>
-      {/* <StatusBar
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
-      /> */}
+      />
     </>
   );
 }
--- mobile/src/screens/Home.jsx
+++ mobile/src/screens/Home.jsx
@@ -6,19 +6,27 @@ export default function Home(){
     const navigation = useNavigation();
 
     return(
-        <View>
-            <View>
-                <Image source={require('../assets/logo2.png')} />
+        <View style={styles.docker}>
+            <View style={styles.container}>
+                <Image source={require('../assets/logo2.png')} style={{width: 100, height: 100}} />
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    TouchableOpacity : {
-        backgroundColor: 'blue',
-        borderRadius: 10,
-        width: 300,
-        height: 200
+    docker: {
+        backgroundColor: '#efefef',
+        width: '100%',
+        height: '100%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    container: {
+        width: '80%',
+        height: '75%',
+        backgroundColor: '#fff',
+        borderRadius: 30
     }
 });
