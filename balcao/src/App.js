e28627a2d140f092a978ae5dfaa2248e3a3c4943
Alan Santana Leão <112034310+aln2006@users.noreply.github.com>
2023-03-14 01:42:47 +0000

    Front_end tela de login

---
+++ balcao/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <User_Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ balcao/src/components/User/User_Login.jsx
@@ -0,0 +1,22 @@
+import './User_Login.css';
+export default function Login()
+ {
+     return (
+     <div>
+        <div className="header">
+            <img src={logo} alt="" className="logo" />
+              <h1 className="title">Pizzeria Admin</h1>
+        </div>
+         <div className="container">
+            <div className="block">
+                <h1 className="info">Sign in with your <br/> Atendent account</h1><br/>
+                    <input type="text" className="input" placeholder="User" name="" id="" /><br/>
+
+                    <input type="text" className="input" placeholder="Password" name="" id="" /><br />
+
+                <a href='http://127.0.0.1:5174/' ><button className="button">Login</button></a>
+            </div>
+        </div>
+    </div>
+)
+}
