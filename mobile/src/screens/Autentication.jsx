0caed9a0dc689c7cc5e7bfb4c300504add0db729
Gabriel Matiolli <97938132+m4tiolli@users.noreply.github.com>
2023-03-14 00:32:18 +0000

    bla

--- mobile/src/screens/Autentication.jsx
+++ mobile/src/screens/Autentication.jsx
@@ -70,11 +70,18 @@ export default function Autentication() {
   }
 
   return (
+<<<<<<< HEAD
     <View>
       <Text id="login" onPress={Alternar(log)}>Login</Text>
       <Text id="register" onPress={() => {RegisterScreen}}>Register</Text>
       <View id="autenticar" render>
       <Section />
+=======
+    <View style={styles.dockerauth}>
+      <View style={styles.boxinput}>
+        <Text style={styles.textinput}>name</Text>
+        <TextInput placeholder="matiolli" style={styles.input} />
+>>>>>>> 1d3c477 (bla)
       </View>
     </View>
   );
@@ -87,4 +94,53 @@ const styles = StyleSheet.create({
     width: 300,
     height: 200,
   },
+<<<<<<< HEAD
+=======
+  buttons: {
+    width: "70%",
+    height: '5%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: 'center',
+    backgroundColor: '#efefef'
+  },
+  text: {
+    fontFamily: "Poppins_600SemiBold",
+    color: "#8E1C1A",
+    fontSize: 20,
+    letterSpacing: 1,
+  },
+  dockerauth: {
+    width: '100%',
+    height: '40%',
+    backgroundColor: '#efefef',
+    alignItems: 'center'
+  },
+  boxinput: {
+    width: "50%",
+    height: 40,
+    borderWidth: 3,
+    borderRadius: 5,
+    borderColor: "#8E1C1A",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textinput: {
+    top: -14,
+    left: 10,
+    color: '#8E1C1A',
+    backgroundColor: '#efefef',
+    fontFamily: 'Poppins_600SemiBold',
+    position: 'absolute',
+    padding: 2,
+  },
+  input: {
+    width: '90%',
+    height: '85%',
+    borderRadius: 2,
+    backgroundColor: '#efefef',
+    outlineStyle: 'none'
+
+  }
+>>>>>>> 1d3c477 (bla)
 });
