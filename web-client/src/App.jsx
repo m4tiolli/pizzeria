d70b9e5106c844cd70b07011c817902f9e7233b7
IsaqueZaack <112034395+IsaqueZaack@users.noreply.github.com>
2023-03-20 17:47:27 +0000

    Mexi no Web-Cliente

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -5,8 +5,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
+  return (<div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
---
+++ web-client/src/components/home/home.jsx
@@ -0,0 +1,29 @@
+/*import "./home.css"
+import reactLogo from './assets/react.svg'
+
+export default function Home (){
+    return(
+        <div>
+            <div ClassName="header">
+                <img src={logo} className="logo"/>
+                <button className="btnLogin">Login</button>
+                <button className="btnCadastrar">Cadastrar</button>
+            </div>
+
+            <H1>Bem vindo a nossa pizzaria. Escolha uma pizza e seja feliz!</H1>
+
+            <div className="pesquisar"></div>
+            //Componente da barra de pesquisa
+            <button className="btnPesquisar"></button>
+
+            <div className="btnCardaDeli">
+                <button className="btnCard">Ver o cardápio</button>
+                <button className="btnDeli">Cadastrar endereço</button>
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    )
+}*/
