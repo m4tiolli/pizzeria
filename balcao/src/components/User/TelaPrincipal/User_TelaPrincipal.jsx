bc0d391f7cb2681c12fd910462df2ac566c11e57
Alan Santana Leão <112034310+aln2006@users.noreply.github.com>
2023-04-12 01:22:32 +0000

    alterações

--- balcao/src/components/User/TelaPrincipal/User_TelaPrincipal.jsx
+++ balcao/src/components/User/TelaPrincipal/User_TelaPrincipal.jsx
@@ -1,53 +1,55 @@
- import "./User_TelaPrincipal.css";
- import logo from "../../../assets/logo.png"
+import "./User_TelaPrincipal.css";
+import logo from "../../../assets/logo.png";
+import pizzas from'./Pizzas.js';
 //  import menu_hamburger from "../../../assets/menu_hamburger.png"
 
- export default function Tela_principal()
-{   
-     return (
-   <div>
+export default function Tela_principal() {
+
+   
+   return (
+      <div>
 
          <div className="header">
             <button className=""></button>
-         <img src={logo} alt="" className="logo" />
-              <h1 className="title">Pizzeria Balcão</h1>
-              <button className="buttonTitle"> Delivery </button>   
-              <button className="buttonTitle"> Pedidos Para Retirar </button>  
-              <button className="buttonTitle"> Mesas Disponíveis </button> 
-              <button className="buttonTitle"> Estoque </button> 
-              <button className="buttonTitle"> Chat </button> 
-              <button className="ButtonTitle" align="right"> User </button>
-        </div>
+            <img src={logo} alt="" className="logo" />
+            <h1 className="title">Pizzeria Balcão</h1>
+            <button className="buttonTitle"> Delivery </button>
+            <button className="buttonTitle"> Pedidos Para Retirar </button>
+            <button className="buttonTitle"> Mesas Disponíveis </button>
+            <button className="buttonTitle"> Estoque </button>
+            <button className="buttonTitle"> Chat </button>
+            <button className="ButtonTitle" align="right"> User </button>
+         </div>
 
-        <main  align="center">
+         <main align="center">
 
-               <div className="input_pesquisar"><input className="input" type="text"placeholder="Pesquisar item"/></div>
-         <div className="pizza-area"></div>
-        </main>
+            <div className="input_pesquisar"><input className="input" type="text" placeholder="Pesquisar item" /></div>
+            <div className="pizza-area"></div>
+         </main>
 
          <div className="models">
 
             <div className="buttons" style={{ display: "flex", justifyContent: "flex-end" }}>
-               <button className="button">Pizzas</button> 
-               <button className="button">Bebidas</button> 
-               <button className="button">Aperitivos</button> 
+               <button className="button">Pizzas</button>
+               <button className="button">Bebidas</button>
+               <button className="button">Aperitivos</button>
                <button className="button">Promoções</button>
 
             </div>
 
             <div className="pizza-item">
                <a href="">
-                  <div className="pizza-item--img"><img src=""/></div>
+                  <div className="pizza-item--img"><img src="" /></div>
                   <div className="pizza-item--add">+</div>
                </a>
                <div className="pizza-item--price">R$ --</div>
                <div className="pizza-item--name">--</div>
                <div className="pizza-item--desc">--</div>
-             </div>
+            </div>
 
 
          </div>
-    </div>
-    
- )
- }
+      </div>
+
+   )
+}
