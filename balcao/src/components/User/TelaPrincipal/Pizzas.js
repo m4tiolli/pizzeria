b446ce25199dcbcc8f72c68c10198501e21a512a
Alan Santana Leão <112034310+aln2006@users.noreply.github.com>
2023-04-14 01:46:36 +0000

    Atualizações na tela principal

--- balcao/src/components/User/TelaPrincipal/Pizzas.js
+++ balcao/src/components/User/TelaPrincipal/Pizzas.js
@@ -1,243 +1,98 @@
 let pizzaJson = [
+  {
+      id: 1,
+      name: 'Mussarela',
+      img: '../../../assets/pizza.png',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, camada dupla de mussarela e orégano'
+    },
     {
-        "Vegetarian": [
-          {
-            "id": 10,
-            "name": "Vegan Veggie",
-            "price": "23.95",
-            "menu_description": "Daiya vegan mozzarella, paired with fresh veggies",
-            "classifications": {
-              "vegetarian": true
-            },
-            "assets": {
-              "menu": [
-                {
-                  "url": "https://assets.zumepizza.com/public/j4lb8iry.jpg"
-                }
-              ],
-              "product_details_page": [
-                {
-                  "url": "https://assets.zumepizza.com/public/aq89hll9.jpg"
-                }
-              ]
-            },
-            "toppings": [
-              {
-                "id": 50,
-                "name": "Baby Bell Peppers",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/7650vx0h.png"
-                }
-              },
-              {
-                "id": 6,
-                "name": "Basil",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/9pb5d2k4.png"
-                }
-              },
-              {
-                "id": 110,
-                "name": "Daiya Vegan Mozzarella",
-                "description": "FILTERED WATER, TAPIOCA FLOUR, EXPELLER PRESSED NON-GMO CANOLA AND/OR SAFFLOWER OIL, COCONUT OIL, PEA PROTEIN, SALT, VEGAN NATURAL FLAVOURS, INACTIVE YEAST, VEGETABLE GLYCERIN, XANTHAN GUM, CITRIC ACID, TITANIUM DIOXIDE",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/oo9dpuia.png"
-                }
-              },
-              {
-                "id": 74,
-                "name": "Kalamata Olives",
-                "description": "KALAMATA OLIVES, WATER, SEA SALT, SUNFLOWER OIL",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/ezuum3ch.png"
-                }
-              },
-              {
-                "id": 2,
-                "name": "Oregano",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/1dlixxge.png"
-                }
-              },
-              {
-                "id": 75,
-                "name": "Red Onions",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/j8cy3gh3.png"
-                }
-              },
-              {
-                "id": 26,
-                "name": "Roasted Garlic",
-                "description": "PEELED GARLIC, CANOLA OIL",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/s4o6jsl6.png"
-                }
-              },
-              {
-                "id": 1,
-                "name": "Tomato Sauce",
-                "description": "VINE-RIPENED TOMATOES, FRESH BASIL LEAF, SALT, CALCIUM CHLORIDE, NATURALLY DERIVED CITRIC ACID, EXTRA VIRGIN OLIVE OIL, SEA SALT",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/3i572qur.png"
-                }
-              }
-            ]
-          },
-          {
-            "id": 11,
-            "name": "Veggie Zupreme",
-            "price": "19.95",
-            "menu_description": "The freshest, locally sourced veggies",
-            "classifications": {
-              "vegetarian": true
-            },
-            "assets": {
-              "menu": [
-                {
-                  "url": "https://assets.zumepizza.com/public/nfjqscxz.jpg"
-                }
-              ],
-              "product_details_page": [
-                {
-                  "url": "https://assets.zumepizza.com/public/aq89hll9.jpg"
-                }
-              ]
-            },
-            "toppings": [
-              {
-                "id": 121,
-                "name": "Asiago",
-                "description": "CULTURED MILK, ENZYMES, SALT",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/gnhvc83u.png"
-                }
-              },
-              {
-                "id": 50,
-                "name": "Baby Bell Peppers",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/7650vx0h.png"
-                }
-              },
-              {
-                "id": 33,
-                "name": "Basil Chiffonade",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/bs4fw4um.png"
-                }
-              },
-              {
-                "id": 74,
-                "name": "Kalamata Olives",
-                "description": "KALAMATA OLIVES, WATER, SEA SALT, SUNFLOWER OIL",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/ezuum3ch.png"
-                }
-              },
-              {
-                "id": 5,
-                "name": "Mozzarella",
-                "description": "PASTEURIZED MILK, CHEESE CULTURE, SALT, ENZYMES",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/rv3erpfq.png"
-                }
-              },
-              {
-                "id": 2,
-                "name": "Oregano",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/1dlixxge.png"
-                }
-              },
-              {
-                "id": 75,
-                "name": "Red Onions",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/j8cy3gh3.png"
-                }
-              },
-              {
-                "id": 26,
-                "name": "Roasted Garlic",
-                "description": "PEELED GARLIC, CANOLA OIL",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/s4o6jsl6.png"
-                }
-              },
-              {
-                "id": 1,
-                "name": "Tomato Sauce",
-                "description": "VINE-RIPENED TOMATOES, FRESH BASIL LEAF, SALT, CALCIUM CHLORIDE, NATURALLY DERIVED CITRIC ACID, EXTRA VIRGIN OLIVE OIL, SEA SALT",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/3i572qur.png"
-                }
-              }
-            ]
-          },
-          {
-            "id": 12,
-            "name": "The O.G.",
-            "price": "16.95",
-            "menu_description": "The Margherita—with California flair",
-            "classifications": {
-              "vegetarian": true
-            },
-            "assets": {
-              "menu": [
-                {
-                  "url": "https://assets.zumepizza.com/public/u9uqpymh.jpg"
-                }
-              ],
-              "product_details_page": [
-                {
-                  "url": "https://assets.zumepizza.com/public/95n7xd91.jpg"
-                }
-              ]
-            },
-            "toppings": [
-              {
-                "id": 6,
-                "name": "Basil",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/9pb5d2k4.png"
-                }
-              },
-              {
-                "id": 5,
-                "name": "Mozzarella",
-                "description": "PASTEURIZED MILK, CHEESE CULTURE, SALT, ENZYMES",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/rv3erpfq.png"
-                }
-              },
-              {
-                "id": 2,
-                "name": "Oregano",
-                "description": null,
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/1dlixxge.png"
-                }
-              },
-              {
-                "id": 1,
-                "name": "Tomato Sauce",
-                "description": "VINE-RIPENED TOMATOES, FRESH BASIL LEAF, SALT, CALCIUM CHLORIDE, NATURALLY DERIVED CITRIC ACID, EXTRA VIRGIN OLIVE OIL, SEA SALT",
-                "asset": {
-                  "url": "https://assets.zumepizza.com/public/3i572qur.png"
-                }
-              }
-            ]
-          }
-        ]
-      }
-]
+      id: 2,
+      name: 'Calabresa',
+      img: '../../../assets/pizza2.png',
+      price: [21.00, 24.00, 26.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+    },
+    {
+      id: 3,
+      name: 'Quatro Queijos',
+      img: '../../../assets/pizza3.png',
+      price: [23.00, 26.00, 28.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+    },
+    {
+      id: 4,
+      name: 'Brasileira',
+      img: '../../../assets/pizza4.png',
+      price: [25.00, 28.00, 30.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+    },
+    {
+      id: 5,
+      name: 'Portuguesa',
+      img: '../../../assets/pizza5.png',
+      price: [24.00, 27.00, 29.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+    },
+    {
+      id: 6,
+      name: 'Moda da Casa',
+      img: '../../../assets/pizza6.png',
+      price: [30.00, 33.00, 35.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+    },
+    {
+      id: 7,
+      name: 'Banana com canela',
+      img: '../../../assets/pizza7.png',
+      price: [27.00, 30.00, 32.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Mussarela, banana, canela e açúcar'
+    },
+    {
+      id: 8,
+      name: 'Chocolate com morango',
+      img: '../../../assets/pizza8.png',
+      price: [30.00, 32.00, 35.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Creme de leite, lascas de chocolate e morangos'
+    }
+];
--- balcao/src/components/User/TelaPrincipal/User_TelaPrincipal.jsx
+++ balcao/src/components/User/TelaPrincipal/User_TelaPrincipal.jsx
@@ -1,6 +1,8 @@
 import "./User_TelaPrincipal.css";
 import logo from "../../../assets/logo.png";
 import pizzas from'./Pizzas.js';
+import "./Pizzas.js";
+import "./Script.js"
 //  import menu_hamburger from "../../../assets/menu_hamburger.png"
 
 export default function Tela_principal() {
@@ -34,6 +36,7 @@ export default function Tela_principal() {
                <button className="button">Bebidas</button>
                <button className="button">Aperitivos</button>
                <button className="button">Promoções</button>
+               <div class="menu-openner"><span>0</span>🛒</div>
 
             </div>
 
@@ -47,9 +50,79 @@ export default function Tela_principal() {
                <div className="pizza-item--desc">--</div>
             </div>
 
+            <div class="cart--item">
+               <img src="" />
+               <div class="cart--item-nome">--</div>
+                  <div class="cart--item--qtarea">
+                  <button class="cart--item-qtmenos">-</button>
+                  <div class="cart--item--qt">1</div>
+                <button class="cart--item-qtmais">+</button>
+               </div>
+            </div>
 
          </div>
+
+         {/*  aside do carrinho  */}
+    <aside>
+        <div class="cart--area">
+            <div class="menu-closer">❌</div>
+            <h1>Suas Pizzas</h1>
+            <div class="cart"></div>
+            <div class="cart--details">
+                <div class="cart--totalitem subtotal">
+                    <span>Subtotal</span>
+                    <span>R$ --</span>
+                </div>
+                <div class="cart--totalitem desconto">
+                    <span>Desconto</span>
+                    <span>R$ --</span>
+                </div>
+                <div class="cart--totalitem total big">
+                    <span>Total</span>
+                    <span>R$ --</span>
+                </div>
+                <div class="cart--finalizar">Finalizar a compra</div>
+            </div>
+        </div>
+    </aside>
+
+    {/* janela modal .pizzaWindowArea */}
+    <div class="pizzaWindowArea">
+        <div class="pizzaWindowBody">
+            <div class="pizzaInfo--cancelMobileButton">Voltar</div>
+            <div class="pizzaBig">
+                <img src="" />
+            </div>
+            <div class="pizzaInfo">
+                <h1>--</h1>
+                <div class="pizzaInfo--desc">--</div>
+                <div class="pizzaInfo--sizearea">
+                    <div class="pizzaInfo--sector">Tamanho</div>
+                    <div class="pizzaInfo--sizes">
+                        <div data-key="P" class="pizzaInfo--size">PEQUENA <span>--</span></div>
+                        <div data-key="M" class="pizzaInfo--size">MÉDIA <span>--</span></div>
+                        <div data-key="G" class="pizzaInfo--size selected">GRANDE <span>--</span></div>
+                    </div>
+                </div>
+                <div class="pizzaInfo--pricearea">
+                    <div class="pizzaInfo--sector">Preço</div>
+                    <div class="pizzaInfo--price">
+                        <div class="pizzaInfo--actualPrice">R$ --</div>
+                        <div class="pizzaInfo--qtarea">
+                            <button class="pizzaInfo--qtmenos">-</button>
+                            <div class="pizzaInfo--qt">1</div>
+                            <button class="pizzaInfo--qtmais">+</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="pizzaInfo--addButton">Adicionar ao carrinho</div>
+                <div class="pizzaInfo--cancelButton">Cancelar</div>
+            </div>
+        </div>
+    </div>
+
       </div>
+
 
    )
 }
