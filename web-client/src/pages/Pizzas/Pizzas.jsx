1f2a8d3891de82c952b63e076bcd2e419fd48c91
IsaqueZaack <112034395+IsaqueZaack@users.noreply.github.com>
2023-04-25 01:39:05 +0000

    Inicio do back-cardapio

--- web-client/src/pages/Pizzas/Pizzas.jsx
+++ web-client/src/pages/Pizzas/Pizzas.jsx
@@ -1,5 +1,22 @@
 import Header from '../../components/Header/Header'
 
+export default function ListarPizzas(){
+    const navigate = useNavigate();
+    UseEffect(() => {
+        fetch("https://localhost:44383/api/pizza"){
+            method: 
+            "GET",
+        }
+        
+            .then((response) => response.jason())
+            .then((jason) => {setUsuarios(jason);
+            })
+            .catch((error) => {
+                console.log(error);
+                alert("Erro ao buscar Produto");  
+            });
+})
+
 export default function Pizzas(){
     return(
         <div>
