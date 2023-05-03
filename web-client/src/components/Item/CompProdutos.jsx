67611eb7644783d9148057a43aa5d7b572f691eb
IsaqueZaack <112034395+IsaqueZaack@users.noreply.github.com>
2023-05-03 00:35:12 +0000

    Mudanças no cardapio

--- web-client/src/components/Item/CompProdutos.jsx
+++ web-client/src/components/Item/CompProdutos.jsx
@@ -4,10 +4,15 @@ function Produto({ pizza }){
     return(
         <div className='containerPizza'>
             <div className="fundoPizza">
-                <img className='imgPizza' url={pizza.imagem}></img>
+                <img className='imgPizza' src={pizza.imagem}></img>
+            <div>
                 <h1 className='nomePizza'>{pizza.nome}</h1>
                 <h3 className='descricaoPizza' >{pizza.descricao}</h3>
+            <div>
                 <h3 className='precoPizza'>{pizza.preco}</h3>
+                <button></button>
+            </div>
+            </div>
             </div>
         </div>
     )
