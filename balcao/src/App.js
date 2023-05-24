e80672571f2c54e88a978e6f08b7e3056cb3634a
Alan Santana Leão <112034310+aln2006@users.noreply.github.com>
2023-05-24 01:22:02 +0000

    Atualizacao Aside

--- balcao/src/App.js
+++ balcao/src/App.js
@@ -1,7 +1,6 @@
 import Login from './components/User/Login/User_Login';
 import './App.css';
 import Tela_principal from './components/User/TelaPrincipal/User_TelaPrincipal';
-import Tela_Estoque from './components/User/User_TelaEstoque/User_TelaEstoque';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
@@ -10,10 +9,10 @@ function App() {
       path: "/",
       element: <Tela_principal />,
     },
-    {
-      path: "/estoque",
-      element: <Tela_Estoque />,
-    },
+    // {
+    //   path: "/estoque",
+    //   element: <Tela_Estoque />,
+    // },
     {
       path: "/login",
       element: <Login />,
--- balcao/src/components/User/TelaPrincipal/User_TelaPrincipal.jsx
+++ balcao/src/components/User/TelaPrincipal/User_TelaPrincipal.jsx
@@ -13,7 +13,7 @@ export default function Tela_principal() {
     const [asideOpen, setAsideOpen] = useState(false);
 
     useEffect(() => {
-        fetch("https://pizzeria3.azurewebsites.net/api/pizza", {
+        fetch("https://pizzeria2.azurewebsites.net/api/produto", {
             method: "GET",
         })
             .then((response) => response.json())
