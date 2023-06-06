2071d88acf978a07b6afa37df63036652295a4a7
JonathanSoares1234 <jonathanesoares@gmail.com>
2023-06-05 22:22:01 -0300

    Cadastro Mesa

---
+++ api/APIPizzeria/DTO/EconomiaDTO.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APIPizzeria.DTO
+{
+    public class EconomiaDTO
+    {
+        public int ID { get; set; }
+        public double Entradas { get; set; }
+        public double Saidas { get; set; }
+    }
+}
