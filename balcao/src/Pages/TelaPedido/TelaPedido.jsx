4c61c4f776d5fb9a8eb43da9d966d71b0ca2be85
emersonsalles245@gmail.com <otaviorl711@gmail.com>
2023-06-15 22:19:27 -0300

    teal User e tela pedido

---
+++ balcao/src/Pages/TelaPedido/TelaPedido.jsx
@@ -0,0 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { MdOutlineTableRestaurant, MdOutlineDeliveryDining } from 'react-icons/md';
+import { BsFillBagCheckFill } from 'react-icons/bs';
+import { FaRegUser } from 'react-icons/fa';
+import { AiFillHome } from 'react-icons/ai';
+import Pedido from "../../components/Pedido/Pedido";
+import logo from '../../assets/logo.png';
+
+export default function TelaPedido() {
+    const navigate = useNavigate();
+    const iconStyle = { color: 'white' };
+    const [pedidoOpen, setPedidoOpen] = useState(true);
+    function Home() {
+        navigate("/");
+    }
+    function Delivery() {
+        navigate("/delivery");
+    }
+    function Mesas() {
+        navigate("/mesas");
+    }
+    function Retirar() {
+        navigate("/retirar");
+    }
+    function User() {
+        navigate("/user");
+    }
+
+    return (
+        <div>
+            <div id="root">
+                <div className="header">
+                    <img src={logo} alt="" className="logo" />
+                    <h1 className="title">Pizzeria Balcão</h1>
+                    <button className='buttonTitle' onClick={Home}><AiFillHome size={30} style={iconStyle} /></button>
+                    <button className="buttonTitle" onClick={Delivery}> <MdOutlineDeliveryDining size={30} style={iconStyle} /> </button>
+                    <button className="buttonTitle" onClick={Retirar}> <BsFillBagCheckFill size={30} style={iconStyle} /> </button>
+                    <button className="buttonTitle" onClick={Mesas}> <MdOutlineTableRestaurant size={30} style={iconStyle} /> </button>
+                    <button className="buttonTitle" onClick={User}> <FaRegUser size={30} style={iconStyle} /> </button>
+                </div>
+            </div>
+            <div className="conteudoPedido">
+                
+            </div>
+        </div>
+    );
+}
--- balcao/src/Pages/TelaUser/TelaUser.jsx
+++ balcao/src/Pages/TelaUser/TelaUser.jsx
@@ -7,54 +7,57 @@ import logo from '../../assets/logo.png';
 import "./TelaUser.css";
 
 export default function TelaUser() {
-    const navigate = useNavigate();
-    const iconStyle = { color: 'white' };
+  const navigate = useNavigate();
+  const iconStyle = { color: 'white' };
 
-    function Home() {
-        navigate("/");
-      }
-      function Delivery() {
-        navigate("/delivery");
-      }
-      function Mesas() {
-        navigate("/mesas");
-      }
-      function Retirar() {
-        navigate("/retirar");
-      }
-      function User() {
-        navigate("/user");
-      }
-    return (
-        <div>
-          <div id="root">
-                <div className="header">
-                    <img src={logo} alt="" className="logo" />
-                    <h1 className="title">Pizzeria Balcão</h1>
-                    <button className='buttonTitle' onClick={Home}><AiFillHome size={30} style={iconStyle}/></button>
-                    <button className="buttonTitle" onClick={Delivery}> <MdOutlineDeliveryDining size={30} style={iconStyle} /> </button>
-                    <button className="buttonTitle" onClick={Retirar}> <BsFillBagCheckFill size={30} style={iconStyle} /> </button>
-                    <button className="buttonTitle" onClick={Mesas}> <MdOutlineTableRestaurant size={30} style={iconStyle} /> </button>
-                    <button className="buttonTitle" onClick={User}> <FaRegUser size={30} style={iconStyle} /> </button>
-                </div>
-                <div className="descrição">
-                <h1>Administrador Balcão</h1>
-                </div>
-                <div className="containerFaturamento">
-                    <button className="buttonContainers">Caixa</button>
-
-                    <h1>Faturamento do dia:</h1>
-                    <h3> 20.03.2006</h3>
-                    <h3>Semana 5</h3>
-                    <h3>R$ 102.00</h3>
-                </div>
-                <div className="containerBalcao">
-                  <button className="buttonContainers">Usuário</button>
-                  <h1>Responsável pelo Balcão:</h1>
-                  <h3>Julia Linda</h3>
-                  <h3>user2006</h3>
-                </div>
-            </div>
+  function Home() {
+    navigate("/");
+  }
+  function Delivery() {
+    navigate("/delivery");
+  }
+  function Mesas() {
+    navigate("/mesas");
+  }
+  function Retirar() {
+    navigate("/retirar");
+  }
+  function User() {
+    navigate("/user");
+  }
+  return (
+    <div>
+      <div id="root">
+        <div className="header">
+          <img src={logo} alt="" className="logo" />
+          <h1 className="title">Pizzeria Balcão</h1>
+          <button className='buttonTitle' onClick={Home}><AiFillHome size={30} style={iconStyle} /></button>
+          <button className="buttonTitle" onClick={Delivery}> <MdOutlineDeliveryDining size={30} style={iconStyle} /> </button>
+          <button className="buttonTitle" onClick={Retirar}> <BsFillBagCheckFill size={30} style={iconStyle} /> </button>
+          <button className="buttonTitle" onClick={Mesas}> <MdOutlineTableRestaurant size={30} style={iconStyle} /> </button>
+          <button className="buttonTitle" onClick={User}> <FaRegUser size={30} style={iconStyle} /> </button>
         </div>
-    ) 
+        <div className="descrição">
+          <h1>Administrador Balcão</h1>
+        </div>
+        <div className="conteudo">
+
+          <div className="containerFaturamento">
+            <button className="buttonContainers">Caixa</button>
+
+            <h1>Faturamento do dia:</h1>
+            <h3> 20.03.2006</h3>
+            <h3>Semana 5</h3>
+            <h3>R$ 102.00</h3>
+          </div>
+          <div className="containerBalcao">
+            <button className="buttonContainers">Usuário</button>
+            <h1>Responsável pelo Balcão:</h1>
+            <h3>Julia Linda</h3>
+            <h3>user2006</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
