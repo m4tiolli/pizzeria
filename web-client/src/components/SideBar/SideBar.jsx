f4dbb7354f2b07295534e187cbde9a3564b401e3
Gabriel Matiolli <97938132+m4tiolli@users.noreply.github.com>
2023-06-21 17:20:38 +0000

    Back-End Sidebar (Incompleto)

--- web-client/src/components/SideBar/SideBar.jsx
+++ web-client/src/components/SideBar/SideBar.jsx
@@ -1,6 +1,6 @@
 import "./SideBar.css";
 import { slide as Menu } from "react-burger-menu";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
@@ -15,33 +15,74 @@ const theme = createTheme({
 });
 
 function SideBar() {
+  const [pesquisas, setPesquisas] = useState([]);
+  const [checkboxes, setCheckboxes] = useState({
+    grande: false,
+    pequena: false,
+    doce: false,
+    salgada: false,
+    bebida: false,
+    sobremesa: false,
+  });
+  const [pesquisasFormatadas, setPesquisasFormatadas] = useState("");
+
+  const handleCheckboxPress = (checkboxName) => {
+    setCheckboxes((prevState) => ({
+      ...prevState,
+      [checkboxName]: !prevState[checkboxName],
+    }));
+  };
+
+  useEffect(() => {
+    let atLeastOneCheckboxChecked = false;
+    for (const checkboxName in checkboxes) {
+      if (checkboxes[checkboxName]) {
+        atLeastOneCheckboxChecked = true;
+        break;
+      }
+    }
+    const updatedPesquisas = Object.keys(checkboxes).filter(
+      (checkboxName) => checkboxes[checkboxName]
+    );
+    setPesquisas(updatedPesquisas);
+
+    let pesquisasFormatadas = "";
+    if (updatedPesquisas.length === 1) {
+      pesquisasFormatadas = updatedPesquisas[0];
+    } else {
+      pesquisasFormatadas = updatedPesquisas.join(", ");
+    }
+    setPesquisasFormatadas(pesquisasFormatadas);
+  }, [checkboxes]);
+
   return (
     <Menu width={"20%"} isOpen={false} menuClassName="meumenu">
       <h1 id="filterby">filter by: </h1>
       <ThemeProvider theme={theme}>
         <FormGroup>
           <FormControlLabel
-            control={<Checkbox defaultChecked color="primary" />}
+            control={<Checkbox color="primary" />}
             label="pizzas salgadas"
           />
           <FormControlLabel
-            control={<Checkbox defaultChecked color="primary" />}
+            control={<Checkbox color="primary" />}
             label="pizzas doces"
+            onChange={() => handleCheckboxPress("grande")}
           />
           <FormControlLabel
-            control={<Checkbox defaultChecked color="primary" />}
+            control={<Checkbox color="primary" />}
             label="pizzas grandes"
           />
           <FormControlLabel
-            control={<Checkbox defaultChecked color="primary" />}
+            control={<Checkbox color="primary" />}
             label="pizzas pequenas"
           />
           <FormControlLabel
-            control={<Checkbox defaultChecked color="primary" />}
+            control={<Checkbox color="primary" />}
             label="sobremesas"
           />
           <FormControlLabel
-            control={<Checkbox defaultChecked color="primary" />}
+            control={<Checkbox color="primary" />}
             label="bebidas"
           />
         </FormGroup>
--- web-client/src/pages/Produtos/Produtos.jsx
+++ web-client/src/pages/Produtos/Produtos.jsx
@@ -1,39 +1,44 @@
-import { useState, useEffect } from 'react';
-import Header from '../../components/Header/Header';
-import Produto from '../../components/Item/CompProdutos';
+import { useState, useEffect } from "react";
+import Header from "../../components/Header/Header";
+import Produto from "../../components/Item/CompProdutos";
 import "./Produtos.css";
-import SideBar from '../../components/SideBar/SideBar';
+import SideBar from "../../components/SideBar/SideBar";
+import Loading from "../../components/Loading/Loading";
 
 export default function Produtos() {
-    useEffect(() => {
-        fetch("https://pizzeria3.azurewebsites.net/api/produto", {
-            method: "GET",
-        })
+  const [isLoading, setIsLoading] = useState(true);
 
-            .then((response) => response.json())
-            .then((json) => {
-                setProdutos(json);
-            })
-            .catch((error) => {
-                console.log(error);
-                alert("Erro ao buscar Produto");
-            });
-    }, []);
+  useEffect(() => {
+    fetch("https://pizzeria3.azurewebsites.net/api/produto", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setProdutos(json);
+      })
+      .then((json) => {
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Erro ao buscar Produto");
+      });
+  }, []);
 
-    const [produtos, setProdutos] = useState([]);
-    return (
-        <div id="root">
-            <Header />
-            <SideBar />
-            <div className="Produtos-Container">
-                {
-                    produtos.map((produto, index) => (
-                         <Produto produto={produto} key={index} />
-                    ))
-                }
-            </div>
+  const [produtos, setProdutos] = useState([]);
+  return (
+    <div id="root">
+      <Header />
+      <SideBar />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="Produtos-Container">
+          {produtos.map((produto, index) => (
+            <Produto produto={produto} key={index} />
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
-
-
